59ff9d732bd944e17ec6b85765209be18be5df06
Andry925 <andry925@ukr.net>
2025-03-14 14:09:48 +0200

    fix: change sync sqlalchemy engine to async

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from database import init_db
+from models import user_model
 
 app = FastAPI()
 
@@ -7,3 +9,10 @@ app = FastAPI()
 @app.get("/healthy")
 async def healthy():
     return {"healthy": True}
+
+
+@app.on_event("startup")
+async def on_startup():
+    await init_db()
+
+
