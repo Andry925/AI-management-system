dcf5361f6c27fc00972b23d5333d58d58d32e3cd
Andry925 <andry925@ukr.net>
2025-03-16 11:13:47 +0200

    feat: add relation field to a user model

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -1,4 +1,5 @@
 import sqlalchemy as sa
+from sqlalchemy.orm import relationship
 
 from database import Base
 
@@ -10,5 +11,6 @@ class User(Base):
     username = sa.Column(sa.String, nullable=False)
     email = sa.Column(sa.String, nullable=False, unique=True)
     hashed_password = sa.Column(sa.String, nullable=False)
+    notes = relationship("Note", back_populates="user")
     created_at = sa.Column(sa.DateTime)
     updated_at = sa.Column(sa.DateTime)
