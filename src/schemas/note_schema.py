cbc4293a8c53dff7718bd115a4579b760f83e15d
Andry925 <andry925@ukr.net>
2025-03-16 11:14:10 +0200

    feat: implement note schema

---
+++ src/schemas/note_schema.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, Field
+
+
+class NoteSchema(BaseModel):
+    title: str = Field(min_length=5, max_length=100)
+    content: str = Field(min_length=10, max_length=100)
+    priority: int = Field(0, ge=0, le=100)
+
+
+class NoteResponseSchema(BaseModel):
+    id: int = Field(ge=0)
+    title: str = Field(min_length=5, max_length=100)
+    content: str = Field(min_length=10, max_length=100)
+    priority: int = Field(0, ge=0, le=100)
+    user_email: str = Field(min_length=5, max_length=100)
+
+    class Config:
+        orm_mode = True
+
+
+class NoteUpdateSchema(BaseModel):
+    title: str = Field(min_length=5, max_length=100)
+    content: str = Field(min_length=10, max_length=100)
+    priority: int = Field(0, ge=0, le=100)
