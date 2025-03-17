5e0b88a596eb29251ae7ddcd6a19e3cb8613deae
Andry925 <andry925@ukr.net>
2025-03-17 19:01:16 +0200

    refactor: refactor urls inside tests

--- src/tests/test_notes.py
+++ src/tests/test_notes.py
@@ -24,7 +24,7 @@ UPDATED_NOTE_DATA = {
 
 @pytest.mark.asyncio
 async def test_create_note(async_client):
-    response = await async_client.post("/api/v1/notes/new-note", json=NOTE_DATA)
+    response = await async_client.post("/api/v1/notes/", json=NOTE_DATA)
     assert response.status_code == 201
     data = response.json()
     assert data["title"] == NOTE_DATA["title"]
@@ -34,16 +34,16 @@ async def test_create_note(async_client):
 
 @pytest.mark.asyncio
 async def test_create_note_already_exists(async_client, create_random_note):
-    response = await async_client.post("/api/v1/notes/new-note", json=NOTE_DATA)
+    response = await async_client.post("/api/v1/notes/", json=NOTE_DATA)
     assert response.status_code == 400
     assert response.json()["detail"] == "Note already exists"
 
 
 @pytest.mark.asyncio
 async def test_get_my_notes(async_client, create_random_note):
-    response = await async_client.post("/api/v1/notes/new-note", json=NEW_NOTE)
+    response = await async_client.post("/api/v1/notes/", json=NEW_NOTE)
     assert response.status_code == 201
-    get_response = await async_client.get("/api/v1/notes/my-notes/1/10")
+    get_response = await async_client.get("/api/v1/notes/?page=1&per_page=10")
     assert get_response.status_code == 200
     notes = get_response.json()
     assert isinstance(notes, list)
@@ -54,7 +54,7 @@ async def test_get_my_notes(async_client, create_random_note):
 async def test_update_note(async_client, create_random_note):
     note = create_random_note
     note_id = note.id
-    update_resp = await async_client.put(f"/api/v1/notes/note-update/{note_id}", json=UPDATED_NOTE_DATA)
+    update_resp = await async_client.put(f"/api/v1/notes/{note_id}", json=UPDATED_NOTE_DATA)
     assert update_resp.status_code == 200
     updated_note = update_resp.json()
     assert updated_note["title"] == UPDATED_NOTE_DATA["title"]
@@ -65,7 +65,7 @@ async def test_update_note(async_client, create_random_note):
 @pytest.mark.asyncio
 async def test_update_non_existent_note(async_client, create_random_note):
     invalid_id = 343432
-    update_fail = await async_client.put(f"/api/v1/notes/note-update/{invalid_id}", json=UPDATED_NOTE_DATA)
+    update_fail = await async_client.put(f"/api/v1/notes/{invalid_id}", json=UPDATED_NOTE_DATA)
     assert update_fail.status_code == 404
     assert update_fail.json()["detail"] == "Note does not exist"
 
@@ -74,9 +74,9 @@ async def test_update_non_existent_note(async_client, create_random_note):
 async def test_delete_note(async_client, create_random_note):
     note = create_random_note
     note_id = note.id
-    response = await async_client.delete(f"/api/v1/notes/delete-note/{note_id}")
+    response = await async_client.delete(f"/api/v1/notes/{note_id}")
     assert response.status_code == 204
-    response_after_deletion = await async_client.get(f"/api/v1/notes/single-note/{note_id}")
+    response_after_deletion = await async_client.get(f"/api/v1/notes/{note_id}")
     assert response_after_deletion.status_code == 404
     assert response_after_deletion.json()["detail"] == "Note with such id does not exist"
 
@@ -85,7 +85,7 @@ async def test_delete_note(async_client, create_random_note):
 async def test_get_single_note(async_client, create_random_note):
     note = create_random_note
     note_id = note.id
-    response = await async_client.get(f"/api/v1/notes/single-note/{note_id}")
+    response = await async_client.get(f"/api/v1/notes/{note_id}")
     assert response.status_code == 200
     fetched_note = response.json()
     assert fetched_note["title"] == INITIAL_NOTE_DATA["title"]
@@ -97,9 +97,9 @@ async def test_get_note_history(async_client, create_random_note):
     note = create_random_note
     note_id = note.id
     update_request = {"title": "some new title", "content": "More content", "priority": 8}
-    response = await async_client.put(f"/api/v1/notes/note-update/{note_id}", json=update_request)
+    response = await async_client.put(f"/api/v1/notes/{note_id}", json=update_request)
     assert response.status_code == 200
-    history_resp = await async_client.get(f"/api/v1/notes/note/history/{note_id}/1/10")
+    history_resp = await async_client.get(f"/api/v1/notes/{note_id}/history?page=1&per_page=10")
     assert history_resp.status_code == 200
     history_list = history_resp.json()
     assert isinstance(history_list, list)
