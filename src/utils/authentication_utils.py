8339a9c53fb814a9ae7c5f344450baf8f43f172d
Andry925 <andry925@ukr.net>
2025-03-17 12:24:15 +0200

    refactor: apply linter to project files

--- src/utils/authentication_utils.py
+++ src/utils/authentication_utils.py
@@ -23,9 +23,13 @@ async def authenticate_user(db, email: str, password) -> User | bool:
     result = await db.execute(select(User).where(User.email == email))
     user = result.scalar_one_or_none()
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found")
     if not bcrypt_context.verify(password, user.hashed_password):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect password proviced")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect password proviced")
     return user
 
 
@@ -39,10 +43,13 @@ def create_jwt_token(user: User, expires_delta: timedelta) -> str:
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
-def get_current_user(refresh_token: Annotated[str, Depends(oauth2_bearer)]) -> dict:
+def get_current_user(
+        refresh_token: Annotated[str, Depends(oauth2_bearer)]) -> dict:
     payload = decode_refresh_token(refresh_token=refresh_token)
     if not (payload.get("sub") or not payload.get("id")):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid payload")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid payload")
     return payload
 
 
@@ -51,7 +58,10 @@ def decode_refresh_token(refresh_token: str) -> dict:
         payload = jwt.decode(refresh_token, SECRET_KEY, algorithms=[ALGORITHM])
         return payload
     except JWTClaimsError as e:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f'Refresh token is invalid: {str(e)}')
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f'Refresh token is invalid: {
+                str(e)}')
 
     except JWTError as e:
         raise HTTPException(
