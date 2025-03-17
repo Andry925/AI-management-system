d194a2c09e336615ab21eaadd71fd7a983d0dc86
Andry925 <andry925@ukr.net>
2025-03-17 12:22:42 +0200

    feat: add analysis path to main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from database import init_db
-from routers import auth, notes
+from routers import auth, notes, analysis
 from models.user_model import User
 from models.notes_model import Note
 
@@ -20,3 +20,5 @@ async def on_startup():
 
 app.include_router(auth.router)
 app.include_router(notes.router)
+
+app.include_router(analysis.router)
